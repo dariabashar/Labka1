537c6d697c0898c8e89f00444ca1317f7dc72aae
dariabashar <156795148+dariabashar@users.noreply.github.com>
2024-02-24 23:49:18 +0600

    Update 1.py

--- lab4/generators/1.py
+++ lab4/generators/1.py
@@ -1,7 +1,12 @@
-N=int(input())
-def generate_squares(N):
-    for i in range(1, N + 1):
-        yield i**2
-squares_generator = generate_squares(N)
+number = int(input())
+
+
+def generate_squares(upper_bound):
+    for i in range(1, upper_bound + 1):
+        yield i ** 2
+
+
+squares_generator = generate_squares(upper_bound=number)
 for square in squares_generator:
     print(square)
+
