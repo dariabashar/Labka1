7ce9a59c1e6580da61fae126e5f543d6a3b7afc6
dariabashar <156795148+dariabashar@users.noreply.github.com>
2024-02-24 23:49:52 +0600

    Update 2.py

--- lab4/generators/2.py
+++ lab4/generators/2.py
@@ -1,8 +1,9 @@
-def range(n):
+def get_even_numbers(n):
     for i in range(0, n + 1):
         if i % 2 == 0:
             yield i
 
 
-numbers = range(10)
+numbers = get_even_numbers(10)
 print(*numbers, sep=", ")
+
