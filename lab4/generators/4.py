634e19365dc9872c79aff341af05342b39ea3275
dariabashar <156795148+dariabashar@users.noreply.github.com>
2024-02-24 23:50:41 +0600

    Update 4.py

--- lab4/generators/4.py
+++ lab4/generators/4.py
@@ -1,6 +1,14 @@
+
 def squares(start, stop):
     for i in range(start, stop + 1):
         yield i ** 2
 
 
+lower_bound = 10
+upper_bound = 20
+
 print(*squares(10, 20))
+print()
+
+for squared_number in squares(10, 20):
+    print(squared_number, end=" ")
