3fd0248b7d3d98f055b0105669e00f064c1a69b3
dariabashar <156795148+dariabashar@users.noreply.github.com>
2024-03-03 23:45:56 +0600

    Update ex3.py

--- lab6/builtin_functions/ex3.py
+++ lab6/builtin_functions/ex3.py
@@ -1,15 +1,5 @@
 #Write a Python program with builtin function that checks whether a passed string is palindrome or not.
 
-def palindrom_or_not (string):
-    rev = ''.join(reversed(string))
-    return string == rev
-    
-def main():
-    string = input()
-    result = palindrom_or_not(string)
-    
-    print(result)
-    
-if __name__ == "__main__":
-    main()
-    
+my_input_string = input()
+reversed_string = ''.join(reversed(my_input_string))
+print(my_input_string == reversed_string)
