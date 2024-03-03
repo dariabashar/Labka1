8ecfe748292a9bac7382a993baacd9e650d887f6
dariabashar <156795148+dariabashar@users.noreply.github.com>
2024-03-03 23:44:34 +0600

    Update ex2.py

--- lab6/builtin_functions/ex2.py
+++ lab6/builtin_functions/ex2.py
@@ -1,12 +1,14 @@
 #Write a Python program with builtin function that accepts a string and 
 #calculate the number of upper case letters and lower case letters
 
+
 def main():
     string = input("Write your string: ")
     upper_count = sum(1 for c in string if c.isupper())
     lower_count = sum(1 for c in string if c.islower())
     print("Lowercase:", lower_count)
     print("Uppercase:", upper_count)
+
 
 if __name__ == "__main__":
     main()
