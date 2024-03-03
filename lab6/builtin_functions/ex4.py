4b92cef6dc2a96c2c0939b069e94fa6b216c2c6b
dariabashar <156795148+dariabashar@users.noreply.github.com>
2024-03-03 23:46:41 +0600

    Update ex4.py

--- lab6/builtin_functions/ex4.py
+++ lab6/builtin_functions/ex4.py
@@ -1,15 +1,8 @@
+
 import time
 
-def square_root(number, milliseconds):
-    time.sleep(milliseconds / 1000) 
-    result = pow(number, 0.5)
-    return result
+number = int(input("Enter a number: "))
+milliseconds = int(input("Enter milliseconds: "))
+time.sleep(milliseconds / 1000)
 
-def main():
-    number = int(input("Enter a number: "))
-    milliseconds = int(input("Enter milliseconds: "))
-    result = square_root (number, milliseconds)
-    print ( "Square root of", number,  "after", milliseconds, "milliseconds is", result )
-
-if __name__ == "__main__":
-    main()
+print("Square root of", number,  "after", milliseconds, "milliseconds is", pow(number, 0.5))
